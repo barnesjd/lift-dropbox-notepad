115411ae476fecf66f91c386493ae01399dffe7a
Joe Barnes <barnesjd@gmail.com>
2013-11-02 14:18:26 -0500

    Updated our Save button to be ajax and print the message to a logger on the server

--- src/main/scala/code/snippet/DropboxSnippets.scala
+++ src/main/scala/code/snippet/DropboxSnippets.scala
@@ -4,14 +4,21 @@ import net.liftweb._
 import common._
 import util._
 import Helpers._
+import http._
+import SHtml._
+import js._
+import JsCmds._
+import JE._
+import js.jquery.JqJE._
 
 import omniauth.Omniauth._
 
-class DropboxSnippets {
+class DropboxSnippets extends Loggable {
   def signin = currentAuth.map(a => ClearNodes).openOr(PassThru)
   
   def notes = currentAuth.map { a =>
     val user = a.name
-    "textarea *" #> s"Hello $user! Take some notes!"
+    ".notes *" #> s"Hello $user! Take some notes!" &
+    ".save [onclick]" #> ajaxCall(Jq(".notes") ~> JsFunc("val"), (s => logger.info(s)))
   }.openOr(ClearNodes)
 }
